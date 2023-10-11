e15df9a0366e949b268b457e0ce8482ae7e60e12
Bruno Vittoria <brunovittoria1999@hotmail.com>
2023-10-11 20:18:52 -0300

    Dashboard tickets successfuly collected

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -9,6 +9,9 @@ import { Link } from 'react-router-dom'
 import { collection, getDocs, orderBy, limit, startAfter, query} from 'firebase/firestore'
 import { db } from '../../services/firebaseConnection'
 
+import { format } from 'date-fns'
+
+
 import './dashboard.css'
 
 const listRef = collection(db, "chamados")
@@ -18,6 +21,7 @@ export default function Dashboard(){
 
   const [chamados, setChamados] = useState([])
   const [loading, setLoading] = useState(true)
+  const [isempty, setisEmpty] = useState(false)
 
   useEffect(() => {
     async function loadChamados(){
@@ -31,10 +35,55 @@ export default function Dashboard(){
     }
 
     loadChamados();
+
+    return () => {}
   }, [])
 
   async function updateState(querySnapshot){
+    const isCollectionEmpty = querySnapshot.size === 0 
+
+    if(!isCollectionEmpty){
+      let lista = []
+
+      querySnapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          createdFormat: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+          status: doc.data().status,
+          complemento: doc.data().complemento,
+        })
+      })
+
+      setChamados(chamados => [...chamados, ...lista])
+    }
+    else{ //Lista ESTA VAZIA
+        setisEmpty(true)
+    }
     
+  }
+
+  if(loading){
+    return(
+      <div>
+        <Header/>
+
+        <div className='content'>
+          
+          <Title name="Tickets">
+            <FiMessageSquare size={25}/>
+          </Title>
+
+          <div className='container dashboard'>
+            <span>Buscando chamados...</span>
+          </div>
+
+        </div>
+      </div>
+    )
   }
 
   return(
@@ -74,20 +123,28 @@ export default function Dashboard(){
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td data-label="Cliente">Mercado Esquina</td>
-                  <td data-label="Assunto">Suporte</td>
-                  <td data-label="Status">Em Aberto</td>
-                  <td data-label="Cadastrado">12/05/2022</td>
-                  <td data-label="#">
-                    <button className='action' style={{ backgroundColor: '#3583f6'}}>
-                      <FiSearch color='#fff' size={17}/>
-                    </button>
-                    <button className='action' style={{backgroundColor: '#f6a935'}}>
-                      <FiEdit2 color='#FFF' size={17}/>
-                    </button>
-                  </td>
-                </tr>
+                {chamados.map((item, index) => {
+                  return(
+                    <tr key={index}>
+                      <td data-label="Cliente">{item.cliente}</td>
+                      <td data-label="Assunto">{item.assunto}</td>
+                      <td data-label="Status">
+                        <span className='badge' style={{ backgroundColor: '#999'}}>
+                          {item.status}
+                        </span>
+                      </td>
+                      <td data-label="Cadastrado">{item.createdFormat}</td>
+                      <td data-label="#">
+                        <button className='action' style={{ backgroundColor: '#3583f6'}}>
+                          <FiSearch color='#fff' size={17}/>
+                        </button>
+                        <button className='action' style={{backgroundColor: '#f6a935'}}>
+                          <FiEdit2 color='#FFF' size={17}/>
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
             </>
